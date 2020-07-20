5b61b2b19217b26fadcc1582b860a1bd494eae01
Apurv verma <41693158+vapurv@users.noreply.github.com>
2020-07-20 23:34:47 +0530

    Update linear1.py

--- linear1.py
+++ linear1.py
@@ -10,7 +10,7 @@ y = data.iloc[:,-1].values
 from sklearn.model_selection import train_test_split
 x_train,x_test,y_train,y_test= train_test_split(x,y,test_size = 0.2,random_state=0)
 
-#Implementing the linear regression class 
+#Implementing the linear regression class n
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(x_train,y_train)
