686d8ae8e8555c83a2b1b3e6352de5684f816742
Apurv verma <41693158+vapurv@users.noreply.github.com>
2020-08-11 10:45:02 -0700

    Update multiple_linear_regression.py

--- multiple_linear_regression.py
+++ multiple_linear_regression.py
@@ -9,7 +9,7 @@ import pandas as pd
 dataset = pd.read_csv('50_Startups.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
-print(x)
+print(x
 
 # Encoding categorical data
 from sklearn.compose import ColumnTransformer
